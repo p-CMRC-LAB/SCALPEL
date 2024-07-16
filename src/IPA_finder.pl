d7709b6cff3cc49631c10676344b07f0f605152b
Mireya <mplass@idibell.cat>
2024-07-16 14:09:09 +0200

    IPA finder: Support lower case nucleotides

--- src/IPA_finder.pl
+++ src/IPA_finder.pl
@@ -36,7 +36,7 @@ if (defined($help) || ! $fasta_file){
 my %seq = %{read_fasta ($fasta_file)};
 
 foreach my $chr (keys %seq){
-    my $seq = $seq{$chr};
+    my $seq = uc($seq{$chr});
     my $splus=$seq;
     $splus=~tr/ATGCN/10000/;
     my @plus  = @{find_matches ($splus,0)};
